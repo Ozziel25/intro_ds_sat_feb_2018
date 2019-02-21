7647e72fe5395c8977844f2fa5545df556d1ee39
Ozziel25 <47791331+Ozziel25@users.noreply.github.com>
2019-02-21 10:24:18 -0600

    Add files via upload

---
+++ Alumnos/Ozziel Rosales/test.py
@@ -0,0 +1,8 @@
+form sys import argv
+scipt, first,second,third=argv
+
+print("Yhe script s called:",script)
+print("Your first variable is ",first)
+print("You secon Variable si", second )
+print("Your third Variable is ",third)
+
